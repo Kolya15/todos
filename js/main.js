feb422414f32f0ffded60e4c02e5c33200a33899
Nikolay-Front-end <kolya.soroka15@gmail.com>
2020-02-07 15:46:12 +0200

    project

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 let mainInput = document.querySelector('.main-input');
 let section = document.querySelector('.show-Note');
+let section1 = document.querySelector('.completed-note');
+console.log(section1);
 let arrayTodos = [];
-
 
 mainInput.addEventListener('keydown', function (event){
     if (event.code == "Enter") {
         if (mainInput.value.length) {
             let newNote = {
-                id: arrayTodos.length,
+                id: Math.random()*Math.random(),
                 title: mainInput.value,
                 completed: false,
             }
@@ -22,14 +23,89 @@ mainInput.addEventListener('keydown', function (event){
 function showNewNote(newObj) {
     let div = document.createElement('div');
     div.className = "new-note";
-    div.innerHTML =`<input id="${arrayTodos.length}" class="checkbox-note" type="checkbox" onchange="check()" ><p class="title-note">${newObj.title}</p>`;
+    div.id = newObj.id;
+    div.innerHTML =`<input class="checkbox-note" type="checkbox" onchange="check(event)">
+                    <p class="title-note">${newObj.title}</p>
+                    <button class="remove-note" onclick="removeNote(event)">Remove</button>`;
     section.prepend(div);
 }
 
-function check() {
-    target = event.target;
-
-    console.log(target.id);
+function getTodo(event) {
+    const target = event.target;
+    const parentId = target.parentNode.id;
+    return arrayTodos.findIndex(item => item.id == parentId);
 }
+
+function check(event) {
+    if (arrayTodos[getTodo(event)].completed == false){
+        arrayTodos[getTodo(event)].completed = true;
+        showCompletedNote();
+    }
+    else {
+        arrayTodos[getTodo(event)].completed = false;
+    }
+    console.log(arrayTodos);
+
+
+}
+
+function removeNote(event) {
+    event.target.parentNode.remove();
+    arrayTodos.splice(getTodo(event),1);
+    console.log(arrayTodos);
+}
+
+
+
+// function showCompletedNote() {
+// //     let arrayCompletedNote = [];
+// //     arrayTodos.forEach((item) =>{
+// //         if (item.completed == true){
+// //             arrayCompletedNote.push(item);
+// //         }
+// //         console.log(arrayCompletedNote);
+// //         showCompletedArr(arrayCompletedNote);
+// //     });
+// // }
+
+function showCompletedNote() {
+    arrayTodos.forEach((item) =>{
+        if (item.completed == true){
+            let div = document.createElement('div');
+            div.className = "new-note completedNote";
+            div.id = item.id;
+            div.innerHTML =`<input class="checkbox-note" type="checkbox" checked onchange="check(event)">
+                    <p class="title-note">${item.title}</p>
+                    <button class="remove-note" onclick="removeNote(event)">Remove</button>`;
+            section1.prepend(div);
+            item.completed = false;
+            // section1.querySelectorAll('.completedNote').forEach(item => item.remove())
+            // showCompletedArr(arrayCompletedNote);
+        }
+    });
+}
+
+// function showCompletedArr(arrComleted) {
+//     arrComleted.forEach((item) => {
+//
+//         let div = document.createElement('div');
+//         div.className = "new-note";
+//         div.id = item.id;
+//         div.innerHTML =`<input class="checkbox-note" type="checkbox" checked onchange="check(event)">
+//                     <p class="title-note">${item.title}</p>
+//                     <button class="remove-note" onclick="removeNote(event)">Remove</button>`;
+//         section1.prepend(div);
+//         //
+//     })
+// }
+
+let btnCompletedNote = document.querySelector('.btn-completed-note');
+btnCompletedNote.addEventListener('click', function () {
+    section1.style.display = 'block';
+})
+
+
+
+
 
 
